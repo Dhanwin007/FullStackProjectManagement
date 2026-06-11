bc9b7b3f497d55d6635ab4fc3da9c4fec3bc5a3f
Dhanwin007 <dhanwing05@gmail.com>
2026-06-11 20:12:52 +0530

    i think i fixed smtp issue

--- Backend/src/utils/mail.js
+++ Backend/src/utils/mail.js
@@ -61,7 +61,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
